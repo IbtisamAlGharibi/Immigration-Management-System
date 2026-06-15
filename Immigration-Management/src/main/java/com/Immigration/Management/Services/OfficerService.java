39f6aee7b1113d6adeca74f88962bfb79c6f54c3
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-15 14:50:20 +0400

    promoteOfficer() method Done

--- Immigration-Management/src/main/java/com/Immigration/Management/Services/OfficerService.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/Services/OfficerService.java
@@ -1,8 +1,28 @@
 package com.Immigration.Management.Services;
 
+import com.Immigration.Management.Entities.ImmigrationOfficer;
+import com.Immigration.Management.Repositories.OfficerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OfficerService {
 
+    OfficerRepository officerRepository;
+    @Autowired
+    public OfficerService(OfficerRepository officerRepository) {
+        this.officerRepository = officerRepository;
+    }
+
+    public ImmigrationOfficer promoteOfficer(Long officerId, String newRank, int newClearanceLevel ){
+        ImmigrationOfficer immigrationOfficer = new ImmigrationOfficer();
+        officerRepository.getOfficerById(String.valueOf(officerId));
+        if (immigrationOfficer.isActive()){
+        immigrationOfficer.setOfficerRank(newRank);
+        immigrationOfficer.setClearanceLevel(newClearanceLevel);
+        }else {
+            throw new RuntimeException("Officer not exists");
+        }
+       return officerRepository.save(immigrationOfficer);
+    }
 }
