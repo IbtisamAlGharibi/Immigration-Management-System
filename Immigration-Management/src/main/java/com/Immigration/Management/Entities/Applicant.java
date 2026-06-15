5917067c50d145a68a32bbf9fe167a1dc0e2e4ba
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-15 09:38:13 +0400

    Class Applicant is created

---
+++ Immigration-Management/src/main/java/com/Immigration/Management/Entities/Applicant.java
@@ -0,0 +1,19 @@
+package com.Immigration.Management.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Applicant extends Person{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String passportNumber;
+    private String nationality;
+    private boolean criminalRecord;
+
+
+}
