22eb22b1f31c74eab6cd43925127f7a62b88846a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-15 09:32:29 +0400

    Class Person is created

---
+++ Immigration-Management/src/main/java/com/Immigration/Management/Entities/Person.java
@@ -0,0 +1,30 @@
+package com.Immigration.Management.Entities;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Person {
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}
---
+++ Immigration-Management/src/main/java/com/Immigration/Management/ImmigrationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Immigration.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmigrationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmigrationManagementApplication.class, args);
+	}
+
+}
