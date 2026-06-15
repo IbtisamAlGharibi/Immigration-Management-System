dc09434a20d498cd1102e89b79ee5f900c9631ac
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-15 11:10:04 +0400

    CenterRepository  methods Done

--- Immigration-Management/src/main/java/com/Immigration/Management/Repositories/CenterRepository.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/Repositories/CenterRepository.java
@@ -2,6 +2,17 @@ package com.Immigration.Management.Repositories;
 
 import com.Immigration.Management.Entities.ImmigrationCenter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CenterRepository extends JpaRepository<ImmigrationCenter,Long> {
+
+    @Query("select c from ImmigrationCenter c where c.id=:id")
+    ImmigrationCenter findById(@Param("id") String id);
+
+    @Query("select c from ImmigrationCenter c where c.name=:name")
+    ImmigrationCenter findByName(@Param("name") String name);
+
 }
