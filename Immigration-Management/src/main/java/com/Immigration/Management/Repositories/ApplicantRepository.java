c30e900053f37760716973755438869c1e3bdc62
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-15 11:00:47 +0400

    ApplicantRepository methods Done

--- Immigration-Management/src/main/java/com/Immigration/Management/Repositories/ApplicantRepository.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/Repositories/ApplicantRepository.java
@@ -2,6 +2,19 @@ package com.Immigration.Management.Repositories;
 
 import com.Immigration.Management.Entities.Applicant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ApplicantRepository extends JpaRepository<Applicant,Long> {
+
+    @Query("Select a.firstName, a.lastName , a.nationality from Applicant a where a.nationality=:nationality")
+    List<Applicant> findByNationality(@Param("nationality") String nationality);
+
+    @Query("Select a.firstName, a.lastName , a.passportNumber from Applicant a where a.passportNumber=:passportNumber")
+    List<Applicant> findByPassportNumber(@Param("passportNumber") String passportNumber);
+
+    @Query("Select a.firstName, a.lastName , a.id from Applicant a where a.id=:id")
+    List<Applicant> findById(@Param("id") String  id);
 }
