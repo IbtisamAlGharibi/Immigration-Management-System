d08c716289281f6132ab8ed265a5f802e27bbeda
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-15 09:47:38 +0400

    Class immigrationOfficer is created

---
+++ Immigration-Management/src/main/java/com/Immigration/Management/Entities/immigrationOfficer.java
@@ -0,0 +1,24 @@
+package com.Immigration.Management.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class immigrationOfficer extends Person{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String badgeNumber;
+    private String rank;
+    private int clearanceLevel;
+    private boolean isActive;
+
+}
