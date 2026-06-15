ec353e9b29c4c62cf6cad43dd578b95cfb891267
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-15 11:37:57 +0400

    OfficerRepository methods Done

--- Immigration-Management/src/main/java/com/Immigration/Management/Repositories/OfficerRepository.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/Repositories/OfficerRepository.java
@@ -2,6 +2,19 @@ package com.Immigration.Management.Repositories;
 
 import com.Immigration.Management.Entities.ImmigrationOfficer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OfficerRepository extends JpaRepository<ImmigrationOfficer,Long> {
+
+    @Query("select o from ImmigrationOfficer o where o.isActive = true")
+    List<ImmigrationOfficer> getAllOfficers();
+
+    @Query("select o from ImmigrationOfficer o where o.id=:id ")
+    ImmigrationOfficer getOfficerById(@Param("id") String id);
+
+    @Query("select o from ImmigrationOfficer o where o.badgeNumber=:badgeNumber ")
+    ImmigrationOfficer getOfficerByBadgeNumber(@Param("badgeNumber") String badgeNumber);
 }
