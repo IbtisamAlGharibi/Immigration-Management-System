a0ee212d680a9e3f714a8ff94d851fe45bd04718
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-15 16:29:12 +0400

    SubmitApplication() Done

--- Immigration-Management/src/main/java/com/Immigration/Management/Services/VisaApplicationService.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/Services/VisaApplicationService.java
@@ -1,8 +1,13 @@
 package com.Immigration.Management.Services;
 
+import com.Immigration.Management.Entities.Applicant;
+import com.Immigration.Management.Entities.VisaApplication;
+import com.Immigration.Management.Repositories.ApplicantRepository;
 import com.Immigration.Management.Repositories.VisaApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VisaApplicationService {
@@ -11,5 +16,19 @@ public class VisaApplicationService {
     public VisaApplicationService(VisaApplicationRepository visaApplicationRepository) {
         this.visaApplicationRepository = visaApplicationRepository;
     }
+    ApplicantRepository applicantRepository;
+    @Autowired
+    public VisaApplicationService(ApplicantRepository applicantRepository) {
+        this.applicantRepository = applicantRepository;
+    }
 
+    public VisaApplication SubmitApplication(Long applicantId, String visaType){
+        List<Applicant> applicant = applicantRepository.findById(String.valueOf(applicantId));
+        VisaApplication visaApplication = new VisaApplication();
+        if (applicant.getFirst().isCriminalRecord()){
+           visaApplication.setStatus("REJECTED");
+           throw new RuntimeException("Auto rejected due to criminal flag");
+        }
+        return visaApplicationRepository.save(visaApplication);
+    }
 }
