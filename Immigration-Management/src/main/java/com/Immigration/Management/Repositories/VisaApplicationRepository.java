f8675e2fd3517177a01f2f848bf82387e2f20615
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-15 11:40:34 +0400

    VisaApplicationRepository method Done

--- Immigration-Management/src/main/java/com/Immigration/Management/Repositories/VisaApplicationRepository.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/Repositories/VisaApplicationRepository.java
@@ -2,6 +2,10 @@ package com.Immigration.Management.Repositories;
 
 import com.Immigration.Management.Entities.VisaApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VisaApplicationRepository extends JpaRepository<VisaApplication,Long> {
+    @Query("select v from VisaApplication v where v.id=:id ")
+    VisaApplication getVisaById(@Param("id") String id);
 }
