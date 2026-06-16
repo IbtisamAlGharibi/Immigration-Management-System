5365ccc1a6704f40d0aacc7514bf1fc2c74fc98e
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-16 15:29:00 +0400

    convertToDTO Done

--- Immigration-Management/src/main/java/com/Immigration/Management/DTO/BorderControlOfficerDTO.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/DTO/BorderControlOfficerDTO.java
@@ -1,5 +1,6 @@
 package com.Immigration.Management.DTO;
 
+import com.Immigration.Management.Entities.BorderControlOfficer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,4 +21,24 @@ public class BorderControlOfficerDTO {
     private boolean active;
     private String assignedCheckPoint;
     private boolean k9UnitAssigned;
+
+    public static BorderControlOfficerDTO convertToDTO(BorderControlOfficer entity) {
+
+        BorderControlOfficerDTO dto = new BorderControlOfficerDTO();
+        dto.setOfficerId(entity.getId());
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setEmail(entity.getEmail());
+        dto.setPhoneNumber(entity.getPhoneNumber());
+        dto.setGender(entity.getGender());
+        dto.setBadgeNumber(entity.getBadgeNumber());
+        dto.setOfficerRank(entity.getOfficerRank());
+        dto.setClearanceLevel(entity.getClearanceLevel());
+        dto.setActive(entity.isActive());
+        dto.setAssignedCheckPoint(entity.getAssignedCheckPoint());
+        dto.setK9UnitAssigned(entity.isK9UnitAssigned());
+
+        return dto;
+    }
+
 }
