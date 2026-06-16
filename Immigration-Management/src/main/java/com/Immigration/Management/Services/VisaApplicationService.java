d408e42e0fb0c3307b7d98b93c0ef895d284dd73
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-16 09:15:27 +0400

    assignOfficer() Done

--- Immigration-Management/src/main/java/com/Immigration/Management/Services/VisaApplicationService.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/Services/VisaApplicationService.java
@@ -1,8 +1,10 @@
 package com.Immigration.Management.Services;
 
 import com.Immigration.Management.Entities.Applicant;
+import com.Immigration.Management.Entities.ImmigrationOfficer;
 import com.Immigration.Management.Entities.VisaApplication;
 import com.Immigration.Management.Repositories.ApplicantRepository;
+import com.Immigration.Management.Repositories.OfficerRepository;
 import com.Immigration.Management.Repositories.VisaApplicationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,12 +24,32 @@ public class VisaApplicationService {
         this.applicantRepository = applicantRepository;
     }
 
+    OfficerRepository officerRepository;
+    @Autowired
+    public VisaApplicationService(OfficerRepository officerRepository) {
+        this.officerRepository = officerRepository;
+    }
+
     public VisaApplication SubmitApplication(Long applicantId, String visaType){
         List<Applicant> applicant = applicantRepository.findById(String.valueOf(applicantId));
         VisaApplication visaApplication = visaApplicationRepository.getVisaByType(visaType);
         if (applicant.getFirst().isCriminalRecord()){
            visaApplication.setStatus("REJECTED");
            throw new RuntimeException("Auto rejected due to criminal flag");
+        }
+        return visaApplicationRepository.save(visaApplication);
+    }
+
+    public VisaApplication assignOfficer(Long visaId,Long officerId){
+        ImmigrationOfficer immigrationOfficer = officerRepository.getOfficerById(String.valueOf(officerId));
+        VisaApplication visaApplication = visaApplicationRepository.getVisaById(String.valueOf(visaId));
+
+        if (visaApplication.getVisaType().equalsIgnoreCase("Asylum")){
+            if (immigrationOfficer.getClearanceLevel()== 4 ||  immigrationOfficer.getClearanceLevel()== 5){
+                visaApplication.setImmigrationOfficer(immigrationOfficer);
+            }else {
+                throw  new RuntimeException("Clearance Level Must Be 4 or 5");
+            }
         }
         return visaApplicationRepository.save(visaApplication);
     }
