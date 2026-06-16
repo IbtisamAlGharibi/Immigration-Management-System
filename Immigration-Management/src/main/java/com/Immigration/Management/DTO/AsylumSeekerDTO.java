a9783fe4c34ef29a62dd953b1a43c5c42870d6da
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-16 15:09:18 +0400

    convertToDTO Done

--- Immigration-Management/src/main/java/com/Immigration/Management/DTO/AsylumSeekerDTO.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/DTO/AsylumSeekerDTO.java
@@ -1,5 +1,6 @@
 package com.Immigration.Management.DTO;
 
+import com.Immigration.Management.Entities.AsylumSeeker;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,4 +21,22 @@ public class AsylumSeekerDTO {
     private String countryOfOrigin;
     private String sponsorOrganization;
 
+
+public static AsylumSeekerDTO convertToDTO(AsylumSeeker entity) {
+
+    AsylumSeekerDTO dto = new AsylumSeekerDTO();
+    dto.setApplicantId(entity.getId());
+    dto.setFirstName(entity.getFirstName());
+    dto.setLastName(entity.getLastName());
+    dto.setEmail(entity.getEmail());
+    dto.setPhoneNumber(entity.getPhoneNumber());
+    dto.setGender(entity.getGender());
+    dto.setPassportNumber(entity.getPassportNumber());
+    dto.setNationality(entity.getNationality());
+    dto.setCriminalRecord(entity.isCriminalRecord());
+    dto.setCountryOfOrigin(entity.getCountryOfOrigin());
+    dto.setSponsorOrganization(entity.getSponsorOrganization());
+
+    return dto;
+}
 }
