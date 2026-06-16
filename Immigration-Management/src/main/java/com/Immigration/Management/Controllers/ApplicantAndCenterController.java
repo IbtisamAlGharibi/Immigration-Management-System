6fbac11b610f1e9fcb06ab81272c14a0e4ea2727
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-16 10:49:22 +0400

    Post and Get mapping for center is Done

--- Immigration-Management/src/main/java/com/Immigration/Management/Controllers/ApplicantAndCenterController.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/Controllers/ApplicantAndCenterController.java
@@ -1,16 +1,27 @@
 package com.Immigration.Management.Controllers;
 
+import com.Immigration.Management.Entities.ImmigrationCenter;
+import com.Immigration.Management.Repositories.CenterRepository;
 import com.Immigration.Management.Services.ApplicantService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ApplicantAndCenterController {
     ApplicantService applicantService;
+    CenterRepository centerRepository;
     @Autowired
     public ApplicantAndCenterController(ApplicantService applicantService) {
         this.applicantService = applicantService;
+        this.centerRepository = centerRepository;
     }
 
-
+    @PostMapping("/addCenter")
+    public ImmigrationCenter createCenter(@RequestBody ImmigrationCenter immigrationCenter){
+        return centerRepository.save(immigrationCenter);
+    }
+    @GetMapping("/{id}")
+    public ImmigrationCenter getCenter(@PathVariable Long id){
+        return centerRepository.getReferenceById(id);
+    }
 }
