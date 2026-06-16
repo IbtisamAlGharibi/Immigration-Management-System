4d76a38fe37b1f349726ffef83e930018c6dafbf
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-16 15:01:59 +0400

    convertToDTO Done

--- Immigration-Management/src/main/java/com/Immigration/Management/DTO/ApplicantDTO.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/DTO/ApplicantDTO.java
@@ -1,5 +1,6 @@
 package com.Immigration.Management.DTO;
 
+import com.Immigration.Management.Entities.Applicant;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,4 +19,18 @@ public class ApplicantDTO {
     private String passportNumber;
     private String nationality;
     private boolean criminalRecord;
+
+    public static ApplicantDTO convertToDTO(Applicant entity) {
+        ApplicantDTO dto = new ApplicantDTO();
+        dto.setApplicantId(entity.getId());
+        dto.setFirstName(entity.getFirstName());
+        dto.setLastName(entity.getLastName());
+        dto.setEmail(entity.getEmail());
+        dto.setPhoneNumber(entity.getPhoneNumber());
+        dto.setGender(entity.getGender());
+        dto.setPassportNumber(entity.getPassportNumber());
+        dto.setNationality(entity.getNationality());
+        dto.setCriminalRecord(entity.isCriminalRecord());
+        return dto;
+    }
 }
