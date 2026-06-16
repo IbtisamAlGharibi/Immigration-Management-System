d47d673523ed3ed710b2c16e223e438e49739083
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-16 15:50:31 +0400

    convertToDTO Done

--- Immigration-Management/src/main/java/com/Immigration/Management/DTO/InterviewDTO.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/DTO/InterviewDTO.java
@@ -1,5 +1,6 @@
 package com.Immigration.Management.DTO;
 
+import com.Immigration.Management.Entities.Interview;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,4 +15,20 @@ public class InterviewDTO {
     private String purpose;
     private Long applicantId;
     private Long officerId;
+
+    public static InterviewDTO convertToDTO(Interview entity) {
+
+        InterviewDTO dto = new InterviewDTO();
+        dto.setInterviewId(entity.getId());
+        dto.setInterviewDate(entity.getInterviewDate());
+        dto.setStatus(entity.getStatus());
+        dto.setPurpose(entity.getPurpose());
+        if (entity.getApplicant() != null) {
+            dto.setApplicantId(entity.getApplicant().getId());
+        }
+        if (entity.getImmigrationOfficer() != null) {
+            dto.setOfficerId(entity.getImmigrationOfficer().getId());
+        }
+        return dto;
+    }
 }
