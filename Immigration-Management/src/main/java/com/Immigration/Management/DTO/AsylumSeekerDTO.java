1a5e26622f0ce1af1cda5d349dcf9cca303da764
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-16 15:05:30 +0400

    AsylumSeekerDTO is created

---
+++ Immigration-Management/src/main/java/com/Immigration/Management/DTO/AsylumSeekerDTO.java
@@ -0,0 +1,23 @@
+package com.Immigration.Management.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AsylumSeekerDTO {
+    private Long applicantId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String gender;
+    private String passportNumber;
+    private String nationality;
+    private boolean criminalRecord;
+    private String countryOfOrigin;
+    private String sponsorOrganization;
+
+}
