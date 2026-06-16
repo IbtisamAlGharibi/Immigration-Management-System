bba3644fbb20b6d3bb32ba98f9d9f369d8a4bfa7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-16 14:03:14 +0400

    InterviewController is created

---
+++ Immigration-Management/src/main/java/com/Immigration/Management/Controllers/InterviewController.java
@@ -0,0 +1,10 @@
+package com.Immigration.Management.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/interview")
+public class InterviewController {
+
+}
