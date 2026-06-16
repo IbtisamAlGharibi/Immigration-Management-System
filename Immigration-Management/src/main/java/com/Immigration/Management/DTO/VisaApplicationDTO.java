d4352e7f53256c09313b478c721f5be0ccc243a7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-16 16:09:02 +0400

    convertToDTO Done

--- Immigration-Management/src/main/java/com/Immigration/Management/DTO/VisaApplicationDTO.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/DTO/VisaApplicationDTO.java
@@ -1,5 +1,6 @@
 package com.Immigration.Management.DTO;
 
+import com.Immigration.Management.Entities.VisaApplication;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,4 +16,19 @@ public class VisaApplicationDTO {
     private Long applicantId;
     private Long officerId;
 
+    public static VisaApplicationDTO convertToDTO(VisaApplication entity) {
+
+        VisaApplicationDTO dto = new VisaApplicationDTO();
+        dto.setVisaApplicationId(entity.getId());
+        dto.setVisaType(entity.getVisaType());
+        dto.setStatus(entity.getStatus());
+        dto.setOfficerNotes(entity.getOfficerNotes());
+        if (entity.getApplicant() != null) {
+            dto.setApplicantId(entity.getApplicant().getId());
+        }
+        if (entity.getImmigrationOfficer() != null) {
+            dto.setOfficerId(entity.getImmigrationOfficer().getId());
+        }
+        return dto;
+    }
 }
