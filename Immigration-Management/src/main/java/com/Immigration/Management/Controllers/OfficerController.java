ed9a08386b6482b6c257cc9724e4d0657b587546
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-17 09:21:12 +0400

    Adding DTO in the OfficerController

--- Immigration-Management/src/main/java/com/Immigration/Management/Controllers/OfficerController.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/Controllers/OfficerController.java
@@ -1,9 +1,12 @@
 package com.Immigration.Management.Controllers;
 
+import com.Immigration.Management.DTO.BorderControlOfficerDTO;
+import com.Immigration.Management.DTO.ImmigrationOfficerDTO;
 import com.Immigration.Management.Entities.BorderControlOfficer;
 import com.Immigration.Management.Entities.ImmigrationOfficer;
 import com.Immigration.Management.Services.OfficerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,23 +18,23 @@ public class OfficerController {
         this.officerService = officerService;
     }
     @PostMapping
-    public ImmigrationOfficer hireOfficer(@RequestBody ImmigrationOfficer officer) {
-        return officerService.saveOfficer(officer);
+    public ResponseEntity<ImmigrationOfficerDTO> hireOfficer(@RequestBody ImmigrationOfficer officer) {
+        return ResponseEntity.ok(ImmigrationOfficerDTO.convertToDTO(officerService.saveOfficer(officer)));
     }
     @PostMapping("/borderOfficer")
-    public BorderControlOfficer hireBorderOfficer(@RequestBody BorderControlOfficer borderOfficer) {
-        return (BorderControlOfficer) officerService.saveOfficer(borderOfficer);
+    public ResponseEntity<BorderControlOfficerDTO> hireBorderOfficer(@RequestBody BorderControlOfficer borderOfficer) {
+        return ResponseEntity.ok(BorderControlOfficerDTO.convertToDTO((BorderControlOfficer) officerService.saveOfficer(borderOfficer)));
     }
     @GetMapping("/{id}")
-    public ImmigrationOfficer getOfficerById(@PathVariable Long id) {
-        return officerService.getOfficerById(String.valueOf(id));
+    public ResponseEntity<ImmigrationOfficerDTO> getOfficerById(@PathVariable Long id) {
+        return ResponseEntity.ok(ImmigrationOfficerDTO.convertToDTO(officerService.getOfficerById(String.valueOf(id))));
     }
     @PutMapping("/{id}/promote")
-    public ImmigrationOfficer promoteOfficer(@PathVariable Long id, @RequestParam String newRank, @RequestParam int clearance) {
-        return officerService.promoteOfficer(id, newRank, clearance);
+    public ResponseEntity<ImmigrationOfficerDTO> promoteOfficer(@PathVariable Long id, @RequestParam String newRank, @RequestParam int clearance) {
+        return ResponseEntity.ok(ImmigrationOfficerDTO.convertToDTO(officerService.promoteOfficer(id, newRank, clearance)));
     }
     @PutMapping("/{id}/transfer/{centerId}")
-    public ImmigrationOfficer transferOfficer(@PathVariable Long id, @PathVariable Long centerId) {
-        return officerService.transferOfficer(id, centerId);
+    public ResponseEntity<ImmigrationOfficerDTO> transferOfficer(@PathVariable Long id, @PathVariable Long centerId) {
+        return ResponseEntity.ok(ImmigrationOfficerDTO.convertToDTO(officerService.transferOfficer(id, centerId)));
     }
 }
