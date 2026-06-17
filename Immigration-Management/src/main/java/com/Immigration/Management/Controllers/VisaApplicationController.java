ebe47e6f22aec66ea52bf5920a9bdf3b18cc07b6
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-06-17 09:26:58 +0400

    Adding DTO in the VisaApplicationController

--- Immigration-Management/src/main/java/com/Immigration/Management/Controllers/VisaApplicationController.java
+++ Immigration-Management/src/main/java/com/Immigration/Management/Controllers/VisaApplicationController.java
@@ -1,8 +1,10 @@
 package com.Immigration.Management.Controllers;
 
+import com.Immigration.Management.DTO.VisaApplicationDTO;
 import com.Immigration.Management.Entities.VisaApplication;
 import com.Immigration.Management.Services.VisaApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,25 +18,25 @@ public class VisaApplicationController {
         this.visaApplicationService = visaApplicationService;
     }
     @PostMapping("/submit/{applicantId}")
-    public VisaApplication submitVisa(@PathVariable Long applicantId, @RequestParam("type") String visaType) {
-        return visaApplicationService.SubmitApplication(applicantId, visaType);
+    public ResponseEntity<VisaApplicationDTO> submitVisa(@PathVariable Long applicantId, @RequestParam("type") String visaType) {
+        return ResponseEntity.ok(VisaApplicationDTO.convertToDTO(visaApplicationService.SubmitApplication(applicantId, visaType)));
     }
     @PutMapping("/{visaId}/assign/{officerId}")
-    public VisaApplication assignOfficer(@PathVariable Long visaId,@PathVariable Long officerId) {
-        return visaApplicationService.assignOfficer(visaId, officerId);
+    public ResponseEntity<VisaApplicationDTO> assignOfficer(@PathVariable Long visaId,@PathVariable Long officerId) {
+        return ResponseEntity.ok(VisaApplicationDTO.convertToDTO(visaApplicationService.assignOfficer(visaId, officerId)));
     }
     @PutMapping("/{visaId}/process")
-    public VisaApplication processVisa(@PathVariable Long visaId, @RequestParam String status,
+    public ResponseEntity<VisaApplicationDTO> processVisa(@PathVariable Long visaId, @RequestParam String status,
                                        @RequestParam String notes) {
-        return visaApplicationService.ProcessVisa(visaId, status, notes);
+        return ResponseEntity.ok(VisaApplicationDTO.convertToDTO( visaApplicationService.ProcessVisa(visaId, status, notes)));
     }
     @GetMapping("/applicant/{applicantId}")
-    public List<VisaApplication> getVisasByApplicant(@PathVariable Long applicantId) {
-        return visaApplicationService.getVisasByApplicantId(applicantId);
+    public ResponseEntity<List<VisaApplicationDTO>> getVisasByApplicant(@PathVariable Long applicantId) {
+        return ResponseEntity.ok(VisaApplicationDTO.convertToDTO(visaApplicationService.getVisasByApplicantId(applicantId)));
     }
 
     @GetMapping("/status/{status}")
-    public List<VisaApplication> getVisasByStatus(@PathVariable String status) {
-        return visaApplicationService.getVisasByStatus(status);
+    public ResponseEntity<List<VisaApplicationDTO>> getVisasByStatus(@PathVariable String status) {
+        return ResponseEntity.ok(VisaApplicationDTO.convertToDTO(visaApplicationService.getVisasByStatus(status)));
     }
 }
